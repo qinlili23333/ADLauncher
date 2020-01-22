bc820432ec37670a7ab34ead01ba71dc737eb21f
琴梨梨 <yeyaohua2010@163.com>
2020-01-22 08:59:05 +0800

    更新延时方式,废弃Timer.exe

--- Launch.bat
+++ Launch.bat
@@ -9,17 +9,15 @@ rem 广告拉取格式,压缩包格式输入True,图片格式输入False,推荐�
 set archive=False
 rem 广告拉取附加指令,默认留空,广告使用curl拉取,如果服务器有特殊要求请在这里附带,参考curl --help
 set curlCommand=
-rem 游戏启动预留时间,指点击游戏exe到游戏窗体出现需要的时间,可多预留,但少预留影响体验
-set prepareTime=6
+rem 游戏启动预留时间(毫秒),指点击游戏exe到游戏窗体出现需要的时间,可多预留,但少预留影响体验
+set prepareTime=6000
 
 
 rem 启动闪屏
-start GameADLauncher.exe
+start GameADLauncher.exe %prepareTime%
 rem 先启动游戏,不影响游戏启动速度
 start %gameExe%
 
-rem 召唤定时器延时杀死闪屏窗口
-start Timer.exe %prepareTime%
 
 rem 异步更新广告内容,关闭广告则跳过
 if "%ADLocation%"=="none" goto Exit

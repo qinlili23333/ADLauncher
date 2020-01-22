a3e003a61c26a17dfd41382839c15d6126f8a966
琴梨梨 <yeyaohua2010@163.com>
2020-01-22 11:17:11 +0800

    更新源码

--- source/GameADLauncher/GameADLauncher/Form1.vb
+++ source/GameADLauncher/GameADLauncher/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+    Private Declare Function timeGetTime Lib "winmm.dll" () As Long
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
             Me.Icon = New Icon("Launcher.ico")
@@ -13,17 +14,34 @@
             MessageBox.Show("默认启动图DefaultSplash.jpg丢失或损坏!", "未找到文件!")
             End
         End Try
-
         Try
             BackgroundImage = System.Drawing.Image.FromFile("ADCache.jpg")
 
         Catch ex As Exception
 
         End Try
+
+
+
     End Sub
 
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         End
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Try
+            Dim Savetime As Double
+
+            Savetime = timeGetTime
+            While timeGetTime < Savetime + Double.Parse(Command)
+
+                Application.DoEvents()
+            End While
+            End
+        Catch ex As Exception
+
+        End Try
     End Sub
 End Class
